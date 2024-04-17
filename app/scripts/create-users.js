eca8943f857ebe7aff7341b761e076f6d922c60f
waynelogan <loganamollo@gmail.com>
2024-04-17 16:18:13 +0300

    create users

--- app/scripts/create-users.js
+++ app/scripts/create-users.js
@@ -7,14 +7,7 @@ function generateRandomID() {
     return Math.random().toString(36).substring(2, 9)
 }
 
-const sampleUser = {
-    name: 'Some One',
-    email: 'som@example.com',
-    password: 'weakPassword',
-    id: generateRandomID()
-}
-
-function createUser(user, endpoint) {
+function createUser(user) {
     axios.put(authURL, user)
         .then(response => {
             console.log(response.data)
@@ -24,23 +17,22 @@ function createUser(user, endpoint) {
         })
 }
 
-createUser(sampleUser, 'http://localhost:3000')
+// Read JSON data from file
+fs.readFile(filepath, 'utf8', (err, data) => {
+    if (err) {
+        console.error('Error reading file:', err)
+        return null
+    }
 
-// // Read JSON data from file
-// fs.readFile(filepath, 'utf8', (err, data) => {
-//     if (err) {
-//         console.error('Error reading file:', err)
-//         return null
-//     }
-
-//     try {
-//         // Parse JSON data into array
-//         const users = JSON.parse(data)
-//         // Print the array
-//         const updatedUsers = users.map( user => ({...user, id: generateRandomID()}))
-//         console.log(updatedUsers)
-//     } catch (error) {
-//         console.error('Error parsing JSON:', error)
-//         return null
-//     }
-// })
+    try {
+        // Parse JSON data into array
+        const users = JSON.parse(data)
+        // Print the array
+        const updatedUsers = users.map( user => ({...user, id: generateRandomID()}))
+        console.log(updatedUsers)
+        updatedUsers.forEach( user => createUser(user))
+    } catch (error) {
+        console.error('Error parsing JSON:', error)
+        return null
+    }
+})
